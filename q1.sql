d37bb1b32ff73f3f789d7c9b1ad15ca864b38a2d
Fong Adam Nicholas <fongadam@teach.cs.toronto.edu>
2017-03-16 00:12:57 -0400

    Fully tested q1

--- q1.sql
+++ q1.sql
@@ -1,51 +1,71 @@
+-- Distributions
+
+SET SEARCH_PATH TO markus;
+DROP TABLE IF EXISTS q1;
+
+-- You must not change this table definition.
+CREATE TABLE q1 (
+	assignment_id integer,
+	average_mark_percent real, 
+	num_80_100 integer, 
+	num_60_79 integer, 
+	num_50_59 integer, 
+	num_0_49 integer
+);
+
+-- You may find it convenient to do this for each of the views
+-- that define your intermediate steps.  (But give them better names!)
+DROP VIEW IF EXISTS intermediate_step CASCADE;
 CREATE VIEW allInfo1 AS
 	SELECT Assignment.assignment_id, AssignmentGroup.group_id FROM Assignment LEFT JOIN AssignmentGroup ON Assignment.assignment_id = AssignmentGroup.assignment_id;
 
 CREATE VIEW allInfo AS
-	SELECT aI1.assignment_id, Result.mark FROM (allinfo1) aI1 LEFT JOIN Result ON aI1.group_id = Result.group_id;  
+	SELECT aI1.assignment_id, Result.mark FROM allInfo1 aI1 LEFT JOIN Result ON aI1.group_id = Result.group_id;  
 
-<!--percents-->
+CREATE VIEW allassign AS
+  SELECT DISTINCT assignment_id FROM Assignment;
+--percents
 
 CREATE VIEW outOf AS
 	SELECT assignment_id, out_of * weight as total FROM RubricItem;
 
 CREATE VIEW totalOutOf AS
-	SELECT assignment_id, sum (total) as ototal FROM outOf oo GROUP BY assignment_id;
+	SELECT assignment_id, sum(total) as ototal FROM outOf oo GROUP BY assignment_id;
 
 CREATE VIEW percents AS
-	SELECT aI.assignment_id, aI.mark/too.ototal as perc FROM allInfo aI JOIN totalOutOf too ON aI.assignment_id = too.assignment_id; 
+	SELECT aI.assignment_id, (aI.mark/too.ototal) * 100 as perc FROM allInfo aI JOIN totalOutOf too ON aI.assignment_id = too.assignment_id; 
 
 
 CREATE VIEW average AS
-	SELECT p.assignment_id, avg(p.perc) as avgg FROM (percents) p Group By p.assignment_id;
+	SELECT p.assignment_id, avg(p.perc) as avgg FROM percents p Group By p.assignment_id;
 
-CREATE VIEW 80to100NoNulls AS
-	SELECT assignment_id, perc FROM (percents) p WHERE p.perc>=80 and p.perc <=100;
+CREATE VIEW to100NoNulls AS
+	SELECT assignment_id, perc FROM percents p WHERE p.perc>=80 and p.perc <=100;
 
-CREATE VIEW 80to100 AS
-	SELECT p.assignment_id, count(p.perc) as num1 FROM (80to100NoNulls) nN RIGHT JOIN (percents) p ON p.assignment_id = nN.assignment_id and p.perc = nN.perc GROUP BY p.assignment_id;  
+CREATE VIEW to100 AS
+	SELECT aa.assignment_id, count(nN.perc) as num1 FROM to100NoNulls nN RIGHT JOIN allassign aa ON aa.assignment_id = nN.assignment_id GROUP BY aa.assignment_id;  
 
 
 	
 
-CREATE VIEW 60to80nNoNulls AS
-	SELECT assignment_id,perc FROM (percents) p2 WHERE p2.perc>=60 and p2.perc <80;
+CREATE VIEW to80NoNulls AS
+	SELECT assignment_id,perc FROM percents p2 WHERE p2.perc>=60 and p2.perc <80;
 
-CREATE VIEW 60to80 AS
-	SELECT p2.assignment_id, count(p2.perc) as num2 FROM (60to80NoNulls) nN RIGHT JOIN (percents) p2 ON p2.assignment_id = nN.assignment_id and p2.perc = nN.perc GROUP BY p2.assignment_id;  
+CREATE VIEW to80 AS
+	SELECT aa.assignment_id, count(nN.perc) as num2 FROM to80NoNulls nN RIGHT JOIN allassign aa ON aa.assignment_id = nN.assignment_id GROUP BY aa.assignment_id;  
 
-CREATE VIEW 50to60nNoNulls AS
-	SELECT assignment_id, perc FROM (percents)p3 WHERE p3.perc>=50 and p3.perc <60;
+CREATE VIEW to60NoNulls AS
+	SELECT assignment_id, perc FROM percents p3 WHERE p3.perc>=50 and p3.perc <60;
 
-CREATE VIEW 50to60 AS
-	SELECT p3.assignment_id, count(p3.perc) as num3 FROM (50to60NoNulls) nN RIGHT JOIN (percents) p3 ON p3.assignment_id = nN.assignment_id and p3.perc = nN.perc GROUP BY p4.assignment_id;  
+CREATE VIEW to60 AS
+	SELECT aa.assignment_id, count(nN.perc) as num3 FROM to60NoNulls nN RIGHT JOIN allassign aa ON aa.assignment_id = nN.assignment_id GROUP BY aa.assignment_id;  
 
-CREATE VIEW 0to50nNoNulls AS
-	SELECT assignment_id, perc FROM (percents)p4 WHERE p4.perc <50;
+CREATE VIEW to50NoNulls AS
+	SELECT assignment_id, perc FROM percents p4 WHERE p4.perc <50;
 
-CREATE VIEW 0to50 AS
-	SELECT p4.assignment_id, count(p4.perc) as num3 FROM (0to50NoNulls) nN RIGHT JOIN (percents) p4 ON p4.assignment_id = nN.assignment_id and p4.perc =nN.perc GROUP BY p4.assignment_id;   
+CREATE VIEW to50 AS
+	SELECT aa.assignment_id, count(nN.perc) as num4 FROM to50NoNulls nN RIGHT JOIN allassign aa ON aa.assignment_id = nN.assignment_id GROUP BY aa.assignment_id;   
 
-INSERT INTO q1(SELECT a.assignment as assignment_id, a.avgg as average_mark_percent, c1.num1 as num_80_100, c2.num2 as num_60_79, c3.num3 as num_50_59, c4.num4 as num_0_49 
-	FROM average a, 80to100 c1, 60to80 c2, 50to60 c3, 0to50 c4 
-	WHERE a.assignment_id = c1.assignment_id and c1.assignment_id = c2.assignment_id and c2.assignment_id = c3.assignment_id and c3.assignment_id = c4.assignment_id);  
+INSERT INTO q1 SELECT a.assignment_id as assignment_id, a.avgg as average_mark_percent, c1.num1 as num_80_100, c2.num2 as num_60_79, c3.num3 as num_50_59, c4.num4 as num_0_49 
+	FROM average a, to100 c1, to80 c2, to60 c3, to50 c4 
+	WHERE a.assignment_id = c1.assignment_id and c1.assignment_id = c2.assignment_id and c2.assignment_id = c3.assignment_id and c3.assignment_id = c4.assignment_id;  
---
+++ testq1.sql
@@ -0,0 +1,38 @@
+insert into Assignment values 
+(1, 'n/a1', '1234-12-12 12:12:12', 1, 5),
+(2, 'n/a2', '1234-12-12 12:12:12', 1, 5),
+(3, 'n/a3', '1234-12-12 12:12:12', 1, 5),
+(4, 'n/a4', '1234-12-12 12:12:12', 1, 5),
+(5, 'n/a5', '1234-12-12 12:12:12', 1, 5),
+(6, 'n/a6', '1234-12-12 12:12:12', 1, 5);
+
+
+insert into AssignmentGroup values
+(1, 1, 'sda'),
+(2, 1, 'sdasd'),
+(3, 2, 'sdasd'),
+(4, 2, 'sdasd'),
+(5, 3, 'asdasda'),
+(6, 4, 'sdasd'),
+(7, 5, 'asdasd');
+
+
+insert into RubricItem values 
+(1, 1, 'style', 10, 0.4),
+(2, 1, 'correctness', 10, 0.6),
+(3, 2, 'style', 10, 0.4),
+(4, 2, 'correctness', 10, 0.6),
+(5, 3, 'style', 10, 0.4),
+(6, 3, 'correctness', 10, 0.6),
+(7, 4, 'style', 10, 0.4),
+(8, 4, 'correctness', 10, 0.6),
+(9, 5, 'style', 10, 0.4),
+(10, 5, 'correctness', 10, 0.6),
+(11, 6, 'style', 10, 1);
+
+insert into Result values
+(1, 9, true),
+(2, 3, true),
+(3, 6, true),
+(4, 7, true),
+(5, 10, true);
